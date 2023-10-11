acb3b47e7202ee2b15f23449b67b6af3dcbe0d05
Sam Geens <sam-geens@hotmail.be>
2023-10-11 15:52:02 +0200

    fix

--- src/entities/language.rs
+++ src/entities/language.rs
@@ -9,9 +9,19 @@ pub struct Language {
     pub name: String,
 }
 
+// impl sqlx::postgres::PgHasArrayType for Language {
+//     fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+//       // Array type name is the name of the element type prefixed with `_`.
+//       sqlx::postgres::PgTypeInfo::with_name("_language")
+//     }
+// }
+
 impl sqlx::postgres::PgHasArrayType for Language {
-    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
-      // Array type name is the name of the element type prefixed with `_`.
-      sqlx::postgres::PgTypeInfo::with_name("_language")
-    }
+  fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+    sqlx::postgres::PgTypeInfo::with_name("_language")
+  }
+
+  fn array_compatible(_ty: &sqlx::postgres::PgTypeInfo) -> bool {
+    true
+  }
 }
